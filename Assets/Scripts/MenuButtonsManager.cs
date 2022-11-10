58e5cfef5ad7e47fa6499c268ea9b1bdeefef462
Juanm4R <110467078+Juanm4R@users.noreply.github.com>
2022-11-10 09:05:32 -0300

    DHFWER

---
+++ Assets/Scripts/MenuButtonsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonsManager : MonoBehaviour
+{
+    bool HasToStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BotonJugar()
+    {
+        SceneManager.LoadScene("Tic L1 L2");
+    }
+
+    public void BotonSalir()
+    {
+        Application.Quit();
+    }
+}
